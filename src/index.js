95880b81ea707eab271f67271b07b2bf57799a91
ritvitz07 <guptaritvik42@gmail.com>
2024-05-08 23:02:22 +0530

    commit_3

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from 'react-dom';        // used to hook/connect our application with index.html
 
 import App from './App';
+import './index.css';
 
  ReactDOM.render(<App />, document.getElementById('root'))
