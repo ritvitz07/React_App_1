2fd64deb4983815d1249b7c265a224e12d9067ff
ritvitz07 <guptaritvik42@gmail.com>
2024-05-10 01:00:10 +0530

    commit_7

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -3,14 +3,8 @@ import './features.css';
 
 const Features = ({ title, text }) => {
   return (
-    <div className='gpt3__features-container__feature'>
-        <div className='gpt3__features-container__feature-title'>
-          <div />
-          <h1>{title}</h1>
-        </div>
-        <div className='gpt3__features-container_feature-text'>
-          {text}
-        </div>
+    <div>
+        Features
     </div>
   )
 }
